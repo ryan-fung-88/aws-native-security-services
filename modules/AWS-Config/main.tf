be77ec1d7a97ca6796328ab7eeed7e5de232f6c1
Fung <ryan.fung@accenture.com>
2025-05-07 11:51:01 -0500

    minor module corrections

--- modules/AWS-Config/main.tf
+++ modules/AWS-Config/main.tf
@@ -1,17 +1,11 @@
 resource "aws_config_configuration_recorder" "config_recorder" {
   name     = var.config_recorder_name
-  role_arn = aws_iam_role.config_recorder.arn  # Replace with the centralized aws config role ARN
+  role_arn = "arn:aws:iam::123456789012:role/MyConfigRole"  # Replace with the centralized aws config role ARN
 
   recording_group {
     all_supported                 = var.recording_all_resource_types
     include_global_resource_types = var.include_global_resource_types
-
-    dynamic "resource_types" {
-      for_each = var.recording_all_resource_types ? [] : var.resource_types_to_record
-      content {
-        resource_types = resource_types.value
-      }
-    }
+    resource_types = var.recording_all_resource_types ? [] : var.resource_types_to_record
   }
 }
 
@@ -20,14 +14,14 @@ resource "aws_config_configuration_recorder_status" "config_recorder_status" {
   is_enabled = var.enable_config_recorder
 
   depends_on = [
-    aws_config_delivery_channel.this,
+    aws_config_delivery_channel.config_delivery_channel,
     aws_config_configuration_recorder.config_recorder,
   ]
 }
 
 resource "aws_config_delivery_channel" "config_delivery_channel" {
   name           = var.delivery_channel_name
-  s3_bucket_name = aws_s3_bucket.config_logs.id  # Replace with the S3 bucket used for centralized logging
+  s3_bucket_name = "Test-bucket"  # Replace with the S3 bucket used for centralized logging
   snapshot_delivery_properties {
     delivery_frequency = var.snapshot_delivery_frequency
   }
