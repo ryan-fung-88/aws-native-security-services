f54b0bc08d31d64b495e69c9acb12f75b169871d
Fung <ryan.fung@accenture.com>
2025-05-07 12:08:45 -0500

    fix

--- aws_config.tf
+++ aws_config.tf
@@ -1,16 +1,16 @@
 ## Option 1
-module "aws_config" {
-  source = "./modules/AWS-Config"
+# module "aws_config" {
+#   source = "./modules/AWS-Config"
 
-  config_recorder_name          = var.config_recorder_name
-  recording_all_resource_types  = var.recording_all_resource_types
-  include_global_resource_types = var.include_global_resource_types
-  resource_types_to_record      = var.resource_types_to_record
-  enable_config_recorder        = var.enable_config_recorder
-  delivery_channel_name         = var.delivery_channel_name
-  snapshot_delivery_frequency   = var.snapshot_delivery_frequency
-  managed_rules                 = var.managed_rules
-}
+#   config_recorder_name          = var.config_recorder_name
+#   recording_all_resource_types  = var.recording_all_resource_types
+#   include_global_resource_types = var.include_global_resource_types
+#   resource_types_to_record      = var.resource_types_to_record
+#   enable_config_recorder        = var.enable_config_recorder
+#   delivery_channel_name         = var.delivery_channel_name
+#   snapshot_delivery_frequency   = var.snapshot_delivery_frequency
+#   managed_rules                 = var.managed_rules
+# }
 
 ## Option 2
 module "aws_config" {
@@ -19,7 +19,7 @@ module "aws_config" {
   config_recorder_name          = "AWS-Config-Recorder"
   recording_all_resource_types  = true
   include_global_resource_types = true
-  #resource_types_to_record = var.resource_types_to_record  // Optional, only if recording_all_resource_types is false
+  #resource_types_to_record = [ "AWS::IAM::User", "AWS::EC2::SecurityGroup" ]  # Specify the resource types you want to record
   enable_config_recorder      = true
   delivery_channel_name       = "AWS-Config-Delivery-Channel"
   snapshot_delivery_frequency = "One_Hour"
