2d51775857887c4014137aa71fe45a1101b5db24
Fung <ryan.fung@accenture.com>
2025-05-22 12:01:53 -0500

    adding SCP to prevent the deletion of wiz access role

--- SCPs.tf
+++ SCPs.tf
@@ -1,7 +1,7 @@
 // This SCP would prevent resources from being deployed if they dont have appropriate tags
 data "aws_iam_policy_document" "deny_resources_without_required_tags" {
   statement {
-    sid = "RequireResourceTags"
+    sid    = "RequireResourceTags"
     effect = "Deny"
 
     actions = [ # add more actions as needed
@@ -15,115 +15,140 @@ data "aws_iam_policy_document" "deny_resources_without_required_tags" {
     ]
 
     condition { # add more conditions as needed
-      test = "ForAnyValue:StringNotEqualsIfExists"
+      test     = "ForAnyValue:StringNotEqualsIfExists"
       variable = "aws:ResourceTag/Environment"
-      values = [ "*" ]
-      }
-     condition {
-      test = "ForAnyValue:StringNotEqualsIfExists"
+      values   = ["*"]
+    }
+    condition {
+      test     = "ForAnyValue:StringNotEqualsIfExists"
       variable = "aws:ResourceTag/Owner"
-      values = [ "*" ]
-      }
+      values   = ["*"]
     }
   }
+}
 
 resource "aws_organizations_policy" "require_resource_tags" {
-  name   = "Require Resource Tags"
+  name        = "Require Resource Tags"
   description = "Deny resources without required tags"
-  type   = "SERVICE_CONTROL_POLICY" 
-  content = data.aws_iam_policy_document.deny_resources_without_required_tags.json
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = data.aws_iam_policy_document.deny_resources_without_required_tags.json
 }
 
 // This SCP would prevent IAM roles and users from being created without a permission boundary
 data "aws_iam_policy_document" "IAM_roles_users_require_permission_boundary" {
   statement {
-    sid = "RequirePermissionBoundary"
+    sid    = "RequirePermissionBoundary"
     effect = "Deny"
 
-    actions = [ 
+    actions = [
       "iam:CreateRole",
       "iam:CreateUser"
     ]
 
-    resources = [ 
+    resources = [
       "arn:aws:iam:::*"
     ]
 
-    condition { 
-      test = "StringNotEqualsIfExists"
+    condition {
+      test     = "StringNotEqualsIfExists"
       variable = "iam:PermissionBoundary"
-      values = [ "*" ] # insert the ARN of the permission boundary policy
-      }
+      values   = ["*"] # insert the ARN of the permission boundary policy
     }
   }
-  
+}
+
 resource "aws_organizations_policy" "require_permission_boundary" {
-  name   = "Require Permission Boundary"
+  name        = "Require Permission Boundary"
   description = "Deny creation of IAM roles and users without permission boundary"
-  type   = "SERVICE_CONTROL_POLICY" 
-  content = data.aws_iam_policy_document.IAM_roles_users_require_permission_boundary.json
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = data.aws_iam_policy_document.IAM_roles_users_require_permission_boundary.json
 }
 
 
 // This SCP would prevent deletion of KMS keys
 data "aws_iam_policy_document" "deny_deletion_of_kms_keys" {
   statement {
-    sid = "PreventKMSKeyDeletion"
+    sid    = "PreventKMSKeyDeletion"
     effect = "Deny"
 
-    actions = [ 
+    actions = [
       "kms:ScheduleKeyDeletion",
       "kms:DeleteAlias",
       "kms:DeleteImportedKeyMaterial"
     ]
 
-    resources = [ 
+    resources = [
       "arn:aws:kms:*:*:key/*"
     ]
 
-    condition { 
-      test = "ArnNotLike"
+    condition {
+      test     = "ArnNotLike"
       variable = "aws:PrincipalArn"
-      values = [ "*" ] # insert the ARN of the security principal
-      }
+      values   = ["*"] # insert the ARN of the security principal
     }
   }
-  
+}
+
 resource "aws_organizations_policy" "deny_deletion_of_kms_keys" {
-  name   = "Deny Deletion of KMS Keys"
+  name        = "Deny Deletion of KMS Keys"
   description = "Prevent deletion of KMS keys"
-  type   = "SERVICE_CONTROL_POLICY" 
-  content = data.aws_iam_policy_document.deny_deletion_of_kms_keys.json
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = data.aws_iam_policy_document.deny_deletion_of_kms_keys.json
 }
 
 // This SCP requries certain AMIs to be used
 data "aws_iam_policy_document" "require_ami" {
   statement {
-    sid = "RequireAMI"
+    sid    = "RequireAMI"
     effect = "Deny"
 
-    actions = [ 
+    actions = [
       "ec2:RunInstances",
       "ec2:CreateLaunchTemplateVersion",
       "ec2:CreateFleet",
       "ec2:RunScheduledInstances"
     ]
 
-    resources = [ 
+    resources = [
       "arn:aws:ec2:::*"
     ]
 
-    condition { 
-      test = "StringNotEqualsIfExists"
+    condition {
+      test     = "StringNotEqualsIfExists"
       variable = "ec2:ImageId"
-      values = [ "*" ] # insert the AMI IDs
-      }
+      values   = ["*"] # insert the AMI IDs
     }
   }
-  
+}
+
 resource "aws_organizations_policy" "require_ami" {
-  name   = "Require AMI"
+  name        = "Require AMI"
   description = "Deny use of non-approved AMIs"
-  type   = "SERVICE_CONTROL_POLICY" 
-  content = data.aws_iam_policy_document.require_ami.json
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = data.aws_iam_policy_document.require_ami.json
+}
+
+// This SCP prevents the deletion of the WizAccess-Role IAM role
+data "aws_iam_policy_document" "prevent_wizaccess_role_deletion" {
+  statement {
+    sid    = "PreventWizAccessRoleDeletion"
+    effect = "Deny"
+
+    actions = [
+      "iam:DeleteRole",
+      "iam:DeleteRolePolicy",
+      "iam:DetachRolePolicy"
+    ]
+
+    resources = [
+      "arn:aws:iam::*:role/WizAccess-Role"
+    ]
+  }
+}
+
+resource "aws_organizations_policy" "require_ami" {
+  name        = "Prevent WizAccess-Role Deletion"
+  description = "Prevent deletion of the WizAccess-Role IAM role"
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = data.prevent_wizaccess_role_deletion.json
 }
